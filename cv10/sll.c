a41dd6be41ffcbbb18de790f18d812633020a8db
David Kozak <david.kozak@oracle.com>
2023-12-13 12:05:38 +0100

    cv10

---
+++ cv10/sll.c
@@ -0,0 +1,53 @@
+#include "sll.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+List list_ctor()
+{
+
+}
+
+Item *item_ctor(Object data)
+{
+
+}
+ 
+
+void list_insert_first(List *list, Item *i)
+{
+
+}
+
+
+bool list_empty(List *list)
+{
+
+}
+ 
+void list_delete_first(List *list)
+{
+
+}
+
+unsigned list_count(List *list)
+{
+
+}
+ 
+
+Item *list_find_minid(List *list)
+{
+
+}
+
+Item *list_find_name(List *list, char *name)
+{
+
+}
+
+void list_dtor(List *list)
+{
+
+}
